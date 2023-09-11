53edbf9224c94bb0e73971c6e8065e126942ca1b
codingTheus <matheusglopes1@gmail.com>
2023-09-11 08:01:26 -0300

    Finalização de estrtura da pagina finish

--- src/Components/SlideFinish/SlideFinish.tsx
+++ src/Components/SlideFinish/SlideFinish.tsx
@@ -1,11 +1,17 @@
 import Style from "./SlideFinish.module.css";
+import Tick from "../../assets/correct.svg";
 
 export const SlideFinish = () => {
   return (
     <>
       <div className={Style.containerSlide}>
         <div className={Style.containerTitle}>
-          <h1>Tudo certo!</h1>
+          <h1>
+            Tudo certo!{" "}
+            <span className={Style.containerTick}>
+              <img src={Tick} alt="Sinal de correto" className={Style.tick} />
+            </span>
+          </h1>
         </div>
         <div className={Style.containerText}>
           <p className={Style.text}>
