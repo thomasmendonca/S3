6eff3ae89eb0bb375d88b7bf212982b5f90f508f
gabriel-a-fernandes <gabriel-a-fernandes@hotmail.com>
2023-09-12 18:37:03 -0300

    Import dos components na pagina home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,24 @@
-import { WelcomeTitle } from "../Components/WelcomeTitle/WelcomeTitle"
+import Style from "../styles/Home.module.css";
+import { NewVehicle } from "../Components/NewVehicleSection/NewVehicleSection";
+import { WelcomeTitle } from "../Components/WelcomeTitle/WelcomeTitle";
+import { CallCasualty } from "../Components/CallCasualtySection/CallCasualtySection";
+import { NavBar } from "../Components/NavBar/NavBar";
 
 export const Home = () => {
-    return(
-        <WelcomeTitle>
-            Bem-vindo(a), Usuário(a)!
-        </WelcomeTitle>
-    )
-}
+  return (
+    <>
+      <div className={Style.menuContainer}>
+        <NavBar />
+      </div>
+      <div className={Style.containerWelcome}>
+        <WelcomeTitle>Usuário(a)</WelcomeTitle>
+      </div>
+      <div className={Style.containerNewVehicle}>
+        <NewVehicle />
+      </div>
+      <div className={Style.containerCasualty}>
+        <CallCasualty />
+      </div>
+    </>
+  );
+};
