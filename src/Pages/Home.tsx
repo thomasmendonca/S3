8eaa01b6c7b9d58f4f81941468454dd65f4cb53a
kauebraz <rm97768@fiap.com.br>
2023-09-11 22:58:16 -0300

    Estilização do component WelcomeTitle

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,7 @@ import { WelcomeTitle } from "../Components/WelcomeTitle/WelcomeTitle"
 export const Home = () => {
     return(
         <WelcomeTitle>
-            Bem-vindo(a), Usuário(a)
+            Bem-vindo(a), Usuário(a)!
         </WelcomeTitle>
     )
 }
